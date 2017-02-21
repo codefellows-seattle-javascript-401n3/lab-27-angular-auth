0329120c85c9971ca9ec4ed18c5fa71f17f497a1
Kyle Winckler <kyle.winckler@gmail.com>
2017-02-20 17:46:02 -0800

    signup design

--- lab-kyle/webpack.config.js
+++ lab-kyle/webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
       {
         test: /\.html$/,
         use: 'html-loader'
+      },
+      {
+        test: /\.(jpg|jpeg|svg|bmp|tiff|gif|png)$/,
+        loader: 'file-loader'
       }
     ]
   },
