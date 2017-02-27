7335cd021b291177c49703ea543baaf3b4bc16a5
Michael Bishop <polishthebishop@gmail.com>
2017-02-26 23:32:53 -0800

    Gallery Posting

--- lab-michael/app/service/gallery-service.js
+++ lab-michael/app/service/gallery-service.js
@@ -2,11 +2,12 @@
 
 module.exports = ['$q', '$log', '$http','authService', galleryService];
 
+
 function galleryService($q, $log, $http, authService) {
   $log.debug('galleryService()');
 
   let service = {};
-  services.gallieres = [];
+  service.galleries = [];
 
   service.createGallery = function(gallery) {
     $log.debug('galleryService.createGallery()');
@@ -23,7 +24,7 @@ function galleryService($q, $log, $http, authService) {
       }
       return $http.post(url, gallery, config);
     })
-    .then(res => {
+    .then( res => {
       $log.log('gallery was created');
       let gallery = res.data;
       service.galleries.unshift(gallery);
@@ -34,5 +35,45 @@ function galleryService($q, $log, $http, authService) {
       return $q.reject(err);
     })
   }
-  
+  service.fetchGalleries = function() {
+    $log.debug('galleryService.fetchGalleries()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+      };
+      return $http.get(url, config);
+    })
+    .then( res => {
+      $log.log('Galleries have been retrieved');
+      service.galleries = res.data;
+      return service.galleries;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err)
+    });
+
+  };
+  // service.deleteGalleries = function(galleryID, galleryData) {
+  //   $log.debug('galleryService.deleteGalleries()');
+  //
+  //   return authService.getToken()
+  //   .then( token => {
+  //
+  //     let url = `${__API_URL__}/api/gallery/${galleryID}`;
+  //     let config = {
+  //       headers: {
+  //         Accept: 'application/json',
+  //         Authorization: `Bearer ${token}`
+  //       }
+  //     };
+  //   });
+  // };
+  return service;
 }
