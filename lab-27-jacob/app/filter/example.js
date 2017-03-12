0a85837447375db81e6ff3c1bd3152a0c461e7f0
jisenber <isenberg.jacob@yahoo.com>
2017-03-12 12:59:27 -0700

    lab34 sorting complete

--- lab-27-jacob/app/filter/example.js
+++
@@ -1,6 +0,0 @@
-let splitMe = function(str) {
-  let pattern = `./*s${str.split('').join('.*')}.*`;
-  return new RegExp(pattern);
-};
-
-//think of: not just uppercasing everything but also the first letter of the word
